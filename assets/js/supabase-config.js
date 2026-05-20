47ead3357a51e4bbcdbeb2b3c17393e6a5e03c2a
Danhj3 <danhj3@gmail.com>
2026-05-20 19:07:26 -0400

    Configure Supabase project URL

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,12 @@
 window.HELLO_BALLOONS_SUPABASE = {
-  url: "https://TU-PROYECTO.supabase.co",
+  url: "https://dcyhfiwuyjydqprkljcu.supabase.co",
   anonKey: "TU_ANON_KEY_PUBLICA"
+};
+
+window.HELLO_BALLOONS_BASE_LOCATION = {
+  name: "Base Hello Balloons - El Palmar",
+  address: "El Palmar, Santa Cruz de la Sierra",
+  latitude: -17.830615,
+  longitude: -63.157275,
+  mapsUrl: "https://maps.app.goo.gl/4rkmYGWY7naXQaFf9"
 };
